0a631d9df5b998c5a0fb2717ba1cafc85e20c7d5
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-06-22 21:39:54 +0900

    added Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "../styles/theme";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <CssBaseline />
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   )
 }
