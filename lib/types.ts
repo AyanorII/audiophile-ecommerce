8609938ce437160810b8ab00611622c2228cba02
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-06-22 22:29:46 +0900

    added Product types

--- lib/types.ts
+++ lib/types.ts
@@ -9,3 +9,39 @@ export type CategoryLink = {
     height: string,
   };
 }
+
+export type ProductImage = {
+  mobile: string;
+  tablet: string;
+  desktop: string;
+}
+
+export type IncludedItem = {
+  quantity: number;
+  item: string;
+}
+
+export type OtherProduct = {
+  slug: string;
+  name: string;
+  image: ProductImage;
+}
+
+export type Product = {
+  id: number,
+  slug: string,
+  name: string,
+  image: ProductImage,
+  category: Category | string,
+  new: boolean,
+  price: number,
+  description: string,
+  features: string,
+  includes: IncludedItem[],
+  gallery: {
+    first: ProductImage
+    second: ProductImage,
+    third: ProductImage,
+  },
+  others: OtherProduct[]
+}
