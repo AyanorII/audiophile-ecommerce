ef3f23e0c7ae7c0635868a8c146e78bad93d39b0
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-07-15 18:09:04 +0900

    add CategoryCardList on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { CategoryCardList } from "@/Category/components";
+import { Container } from "@/components";
 import { Hero } from "@/Home/components";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between">
+		<main className="flex min-h-screen flex-col items-center justify-between gap-10">
 			<Hero />
+			<Container>
+				<CategoryCardList className="mt-10 sm:mt-12 md:mt-32 lg:mt-40" />
+			</Container>
 		</main>
 	);
 }
