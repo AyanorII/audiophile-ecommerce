7f8e840a22c101938f96c31511743f545f924c08
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-07-15 12:57:55 +0900

    🔧 fix(CategoryCard): update CategoryCard component to use tailwind-merge library for class merging 🔧 fix(CategoryCard): update CategoryCard component to add responsive styles for larger screens

--- src/Category/components/CategoryCard/index.tsx
+++ src/Category/components/CategoryCard/index.tsx
@@ -3,19 +3,25 @@ import Link from "next/link";
 
 import { Category } from "@/Category/types";
 import { Button } from "@/components";
+import { twJoin } from "tailwind-merge";
 
 type Props = Category;
 
 export const CategoryCard = ({ type, image, href }: Props) => {
 	return (
 		<Link href={href} className="w-full">
-			<div className="relative flex cursor-pointer flex-col items-center gap-3 rounded-lg bg-gray-100 px-6 pt-20 transition-all duration-300 hover:shadow-md">
+			<div
+				className={twJoin([
+					"relative flex cursor-pointer flex-col items-center gap-3 rounded-lg ",
+					"bg-gray-100 px-6 pt-20 transition-all duration-300 hover:shadow-md md:pt-28",
+				])}
+			>
 				<Image
 					src={image}
 					alt={type}
 					width={200}
 					height={200}
-					className="absolute -top-12 h-full max-w-[150px] object-contain"
+					className="absolute -top-12 h-full max-w-[150px] object-contain md:-top-16 md:max-w-[200px]"
 				/>
 				<div className="relative">
 					<h3 className="text-center font-bold uppercase tracking-widest">
