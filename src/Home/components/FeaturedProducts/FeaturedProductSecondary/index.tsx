039193a90cfa8e15bd4883efe17929e33c2dc192
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-07-16 15:52:37 +0900

    🎨 style(FeaturedProductSecondary): refactor imageClassName to improve readability and maintainability 🎨 style(FeaturedProductTertiary): refactor imageClassName to improve readability and maintainability

--- src/Home/components/FeaturedProducts/FeaturedProductSecondary/index.tsx
+++ src/Home/components/FeaturedProducts/FeaturedProductSecondary/index.tsx
@@ -1,9 +1,18 @@
 import { Button, Card } from "@/components";
+import { twMerge } from "tailwind-merge";
 
 export const FeaturedProductSecondary = () => {
+	const imageClassName =
+		"bg-[url(/images/home/mobile/image-speaker-zx7.jpg)] bg-cover bg-center md:bg-[url(/images/home/tablet/image-speaker-zx7.jpg)] lg:bg-[url(/images/home/desktop/image-speaker-zx7.jpg)]";
+
 	return (
 		<>
-			<Card className="relative flex h-[320px] items-center bg-[url(/images/home/mobile/image-speaker-zx7.jpg)] bg-cover bg-center md:bg-[url(/images/home/tablet/image-speaker-zx7.jpg)] lg:h-[527px] lg:bg-[url(/images/home/desktop/image-speaker-zx7.jpg)]">
+			<Card
+				className={twMerge([
+					"relative flex h-[320px] items-center lg:h-[527px]",
+					imageClassName,
+				])}
+			>
 				<div className="relative ml-[10%] md:ml-[12%] xl:ml-[15%]">
 					<h3 className="mb-8 text-3xl font-bold uppercase tracking-widest">
 						ZX7 Speaker
--- src/Home/components/FeaturedProducts/FeaturedProductTertiary/index.tsx
+++ src/Home/components/FeaturedProducts/FeaturedProductTertiary/index.tsx
@@ -1,9 +1,18 @@
 import { Button, Card } from "@/components";
+import { twMerge } from "tailwind-merge";
 
 export const FeaturedProductTertiary = () => {
+	const imageClassName =
+		"bg-[url(/images/home/mobile/image-earphones-yx1.jpg)] bg-cover md:bg-[url(/images/home/tablet/image-earphones-yx1.jpg)] lg:bg-[url(/images/home/desktop/image-earphones-yx1.jpg)]";
+
 	return (
-		<div className="grid auto-rows-[200px] gap-3 md:auto-rows-[320px] md:grid-cols-2">
-			<Card className="relative bg-[url(/images/home/mobile/image-earphones-yx1.jpg)] bg-cover md:bg-[url(/images/home/tablet/image-earphones-yx1.jpg)] lg:bg-[url(/images/home/desktop/image-earphones-yx1.jpg)]" />
+		<div
+			className={twMerge([
+				"grid auto-rows-[200px] gap-3 md:auto-rows-[320px] md:grid-cols-2",
+				imageClassName,
+			])}
+		>
+			<Card className="relative " />
 			<Card className="flex flex-col items-start justify-center bg-gray-100">
 				<div className="px-3 md:px-5 lg:px-8 xl:px-10">
 					<h3 className="mb-8 text-3xl font-bold uppercase tracking-widest">
