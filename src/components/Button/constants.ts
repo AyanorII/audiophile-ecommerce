cf845cb047feec671ac26bf74d6a9de7ba3c41e5
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-07-16 15:46:49 +0900

    remove background color from outlined Button

--- src/components/Button/constants.ts
+++ src/components/Button/constants.ts
@@ -5,7 +5,7 @@ export const CONTAINED_CLASS_NAME =
 	"bg-primary-main hover:bg-primary-light text-white";
 
 export const OUTLINED_CLASS_NAME =
-	"border-[1px] border-black bg-white hover:bg-black hover:text-white text-black";
+	"border-[1px] border-black hover:bg-black hover:text-white text-black";
 
 export const TEXT_CLASS_NAME =
 	"bg-transparent text-gray-400 hover:text-primary-main";
