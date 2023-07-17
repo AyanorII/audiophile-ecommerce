eede3c756721b445d98330838fad6f0244df08d9
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-07-17 09:37:36 +0900

    fix "LinkList" props

--- src/components/Nav/LinkList/index.tsx
+++ src/components/Nav/LinkList/index.tsx
@@ -4,12 +4,15 @@ import { twMerge } from "tailwind-merge";
 import { NAV_LINKS } from "./constants";
 
 type Props = {
-	linkItemProps: Omit<React.ComponentProps<typeof Link>, "href" | "children">;
+	linkItemProps?: Omit<React.ComponentProps<typeof Link>, "href" | "children">;
 } & React.HTMLAttributes<HTMLUListElement>;
 
 export const LinkList = ({ linkItemProps, className, ...props }: Props) => {
 	return (
-		<ul className={twMerge(["flex flex-col gap-5 md:flex-row", className])}>
+		<ul
+			{...props}
+			className={twMerge(["flex flex-col gap-5 md:flex-row", className])}
+		>
 			{NAV_LINKS.map(({ label, path }) => (
 				<li key={path}>
 					<Link href={path} {...linkItemProps}>
