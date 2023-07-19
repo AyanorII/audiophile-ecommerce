31d98f1529bb3de0435f4862d62837a95870085f
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-07-19 09:40:51 +0900

    add vertical margin to categories layout

--- src/app/categories/[category]/layout.tsx
+++ src/app/categories/[category]/layout.tsx
@@ -14,7 +14,7 @@ const CategoriesLayout = ({ children, params }: Props) => {
 			<div className="bg-zinc-900 pt-[100px]">
 				<CategoryHeader category={params.category} />
 			</div>
-			{children}
+			<div className="my-16 md:my-32 lg:my-40">{children}</div>
 		</div>
 	);
 };
