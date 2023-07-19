7d768d0ffee7966242322de8dcf57d19bb46e3d9
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-07-19 19:12:22 +0900

    add "getFormattedPrice" helper

---
+++ src/lib/helpers/getFormattedPrice.ts
@@ -0,0 +1,7 @@
+export const getFormattedPrice = (price: number) => {
+	return new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: "USD",
+		maximumFractionDigits: 0,
+	}).format(price);
+};
