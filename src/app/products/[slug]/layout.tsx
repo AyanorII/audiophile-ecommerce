8fdaa26302f13fdc0bce067bdeddf07194450585
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-07-22 14:46:55 +0900

    add products layout

---
+++ src/app/products/[slug]/layout.tsx
@@ -0,0 +1,36 @@
+import { CategoryCardList } from "@/Category/components";
+import { About, Container, GoBack } from "@/components";
+
+import { PRODUCTS } from "@/lib/constants";
+
+type Props = {
+	children: React.ReactNode;
+	params: {
+		slug: string;
+	};
+};
+
+const ProductsLayout = ({ children, params }: Props) => {
+	const { slug } = params;
+	const product = PRODUCTS.find(
+		({ slug: productSlug }) => productSlug === slug
+	);
+
+	return (
+		<div className="mt-[100px]">
+			<Container className="mb-16 mt-8 flex flex-col gap-32 md:mb-32 md:mt-24 lg:mb-40 lg:mt-32 lg:gap-40">
+				<div>
+					<GoBack
+						className="my-6 self-start pl-0 md:mb-14"
+						fallbackPath={`/categories/${product?.category}`}
+					/>
+					{children}
+				</div>
+				<CategoryCardList className="mt-10 lg:mt-20" />
+				<About />
+			</Container>
+		</div>
+	);
+};
+
+export default ProductsLayout;
