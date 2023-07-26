f0028cd7e4c70cd41f4763fd419482ad6b32e841
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-07-26 14:04:17 +0900

    add throttle to "useScrollPosition"

--- src/lib/hooks/useScrollPosition.tsx
+++ src/lib/hooks/useScrollPosition.tsx
@@ -5,16 +5,26 @@ import { useEventListener } from "@/lib/hooks";
 /**
  * Returns the current scroll position and whether the user is scrolling up or down.
  * @param {number} [scrollThreshold=0] - Optional. The minimum scroll difference to trigger the scroll event.
+ * @param {number} [throttleTime=100] - Optional. The time in milliseconds to throttle the scroll event.
  * @returns {{isScrollingUp: boolean, isScrollingDown: boolean, scrollPosition: number}}
  */
-export const useScrollPosition = (scrollThreshold = 0) => {
+export const useScrollPosition = (scrollThreshold = 0, throttleTime = 100) => {
 	const [scrollPosition, setScrollPosition] = useState(0);
 	const [isScrollingUp, setIsScrollingUp] = useState(false);
 	const [isScrollingDown, setIsScrollingDown] = useState(false);
 
 	const lastScrollTop = useRef(0);
+	const isThrottled = useRef(false);
 
 	useEventListener("scroll", () => {
+		if (isThrottled.current) return;
+
+		isThrottled.current = true;
+
+		setTimeout(() => {
+			isThrottled.current = false;
+		}, throttleTime);
+
 		const currentScrollTop =
 			window.scrollY || document.documentElement.scrollTop;
 
