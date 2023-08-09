9e2d58c88c555652450c5204bb799793fefbfa30
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-08-10 07:55:37 +0900

    🔧 fix(CartItem/index.tsx): export `CartItemProps` type to allow external usage 🔧 fix(CartItem/index.tsx): add optional `showQuantity` prop to `CartItem` component for conditional rendering of quantity 🔧 fix(CartItemsList/index.tsx): add optional `CartItemProps` prop to `CartItemsList` component to pass props to `CartItem` component

--- src/Cart/CartItem/index.tsx
+++ src/Cart/CartItem/index.tsx
@@ -8,11 +8,12 @@ import { Card, CounterControlButton } from "@/components";
 import { getFormattedPrice } from "@/lib/helpers";
 import { RootState } from "@/lib/redux/store";
 
-type Props = {
+export type CartItemProps = {
 	item: RootState["cart"]["items"][number];
+	showQuantity?: boolean;
 };
 
-export const CartItem = ({ item }: Props) => {
+export const CartItem = ({ item, showQuantity }: CartItemProps) => {
 	const [, startTransition] = useTransition();
 
 	const { product, quantity } = item;
@@ -27,27 +28,31 @@ export const CartItem = ({ item }: Props) => {
 	};
 
 	return (
-		<div key={id} className="flex items-center gap-3">
+		<div key={id} className="flex items-center gap-3 md:gap-4">
 			<Card className="">
 				<Image
 					src={product.image.mobile}
 					alt={name}
 					width={54}
 					height={54}
-					className="w-full max-w-[54px]"
+					className="w-full max-w-[54px] md:max-w-[64px]"
 				/>
 			</Card>
 			<div className="flex flex-col font-bold tracking-wider">
-				<p className="text-sm uppercase">{shortName}</p>
+				<p className="text-sm uppercase md:text-base">{shortName}</p>
 				<span className="text-gray-400">{getFormattedPrice(price)}</span>
 			</div>
-			<CounterControlButton
-				value={quantity}
-				onDecrement={() => startTransition(() => handleRemoveItem())}
-				onIncrement={() => startTransition(() => handleAddItem())}
-				buttonsClassName="p-3 md:p-4 md:py-[7px] lg:px-5"
-				className="ml-auto"
-			/>
+			{showQuantity ? (
+				<span className="ml-auto font-bold text-gray-400">x{quantity}</span>
+			) : (
+				<CounterControlButton
+					value={quantity}
+					onDecrement={() => startTransition(() => handleRemoveItem())}
+					onIncrement={() => startTransition(() => handleAddItem())}
+					buttonsClassName="p-3 md:p-4 md:py-[7px] lg:px-5"
+					className="ml-auto"
+				/>
+			)}
 		</div>
 	);
 };
--- src/Cart/CartItemsList/index.tsx
+++ src/Cart/CartItemsList/index.tsx
@@ -1,16 +1,17 @@
 import { RootState } from "@/lib/redux/store";
 
-import { CartItem } from "../CartItem";
+import { CartItem, CartItemProps } from "../CartItem";
 
 type Props = {
 	items: RootState["cart"]["items"];
+	CartItemProps?: Partial<CartItemProps>;
 };
-export const CartItemsList = ({ items }: Props) => {
+export const CartItemsList = ({ items, CartItemProps }: Props) => {
 	return (
 		<div className="flex flex-col gap-5">
 			{items.length === 0 && <p className="text-center text-lg">Empty cart</p>}
 			{items.map((item) => (
-				<CartItem key={item.product.id} item={item} />
+				<CartItem key={item.product.id} {...CartItemProps} item={item} />
 			))}
 		</div>
 	);
