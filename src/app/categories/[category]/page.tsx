c6209dea0464ad827ae7a1632072023092b4bbd2
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-08-11 08:35:24 +0900

    fix "dynamic server usage" errors in dev mode by turning off static generation and forcing dynamic rendering

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -1,8 +1,12 @@
+export const dynamicParams = false;
+export const dynamic =
+	process.env.NODE_ENV === "production" ? "force-static" : "force-dynamic";
+
 import { ProductPreview } from "@/Products/components";
 
 import { PRODUCTS } from "@/lib/constants";
 
-export const generateStaticParams = () => {
+const staticParams = () => {
 	const uniqueCategories = [
 		...new Set(PRODUCTS.map(({ category }) => category)),
 	];
@@ -10,8 +14,12 @@ export const generateStaticParams = () => {
 	return uniqueCategories.map((category) => ({ category }));
 };
 
+// fix "dynamic server usage" errors in dev mode by turning off static generation and forcing dynamic rendering
+export const generateStaticParams =
+	process.env.NODE_ENV === "production" ? staticParams : undefined;
+
 type Props = {
-	params: ReturnType<typeof generateStaticParams>[number];
+	params: ReturnType<typeof staticParams>[number];
 };
 
 const CategoryPage = ({ params }: Props) => {
--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,22 +1,30 @@
+export const dynamic =
+	process.env.NODE_ENV === "production" ? "force-static" : "force-dynamic";
+export const dynamicParams = false;
+
 import {
 	Product,
 	ProductGallery,
 	ProductInfo,
 	ProductRecommendations,
 } from "@/Products/components";
-import { ProductType } from "@/Products/types";
 
 import { PRODUCTS } from "@/lib/constants";
 
+const staticParams = () => {
+	return PRODUCTS.map(({ slug }) => ({ slug }));
+};
+
+// fix "dynamic server usage" errors in dev mode by turning off static generation and forcing dynamic rendering
+export const generateStaticParams =
+	process.env.NODE_ENV === "production" ? staticParams : undefined;
+
 type Props = {
-	params: { slug: ProductType["slug"] };
+	params: { slug: ReturnType<typeof staticParams>[number]["slug"] };
 };
 
 const ProductPage = ({ params }: Props) => {
-	const product = PRODUCTS.find(({ slug }) => slug === params.slug);
-
-	// TODO: Render not-found 404
-	if (!product) return null;
+	const product = PRODUCTS.find(({ slug }) => slug === params.slug)!;
 
 	return (
 		<div className="flex flex-col gap-20 md:gap-32 lg:gap-40">
